4423ef7f76d55ddbb89cc24bac5afaa4389f2810
nikkicanedo <nikkicanedo@gmail.com>
2016-12-16 06:44:58 +0800

    Initial commit

---
+++ app/src/main/java/com/nikkay/canedo/todolistprelimexam/AboutTask.java
@@ -0,0 +1,11 @@
+package com.nikkay.canedo.todolistprelimexam;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AboutTask extends AppCompatActivity {
+    
+}
